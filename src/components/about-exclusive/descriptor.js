258b2ae81ef72a51d55974c5ad701537192bd9c8
NMGVox <nijeemgreene@gmail.com>
2023-05-02 18:12:47 -0400

    Add modules for 'about' tab's bio section

---
+++ src/components/about-exclusive/descriptor.js
@@ -0,0 +1,41 @@
+import nikobio from '../../data/niko.txt';
+import klobio from '../../data/klonoa.txt';
+import nikoportrait from '../../images/niko.png';
+import klonoaportrait from '../../images/klonoa.png';
+
+let info = {
+    "niko" : {
+        name: "Niko",
+        bio: nikobio,
+        picture: nikoportrait
+    },
+    "klonoa" : {
+        name: "Klonoa",
+        bio: klobio,
+        picture: klonoaportrait
+    },
+};
+
+function showBiography (name) {
+    let div = document.createElement('div');
+    div.className = 'bio-wrapper';
+
+    let title = document.createElement('h2');
+    title.textContent = info[name].name;
+    title.classList = "bio-name"
+    div.appendChild(title);
+
+    let portrait = new Image;
+    portrait.classList.add('portrait');
+    portrait.src = info[name].picture;
+    div.appendChild(portrait);
+
+    let bio = document.createElement('p');
+    bio.textContent = info[name].bio;
+    bio.classList.add('biography');
+    div.appendChild(bio);
+
+    return div;
+}
+
+export { showBiography }
--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,17 @@
 import { removeContent } from "./content-remove.js";
+import { showBiography } from "./about-exclusive/descriptor.js";
 
 function showAbout() {
-    removeContent(document.querySelector('#mid'));
+    let main = document.querySelector('#mid');
+    removeContent(main);
 
     let header = document.createElement('h1');
     header.textContent = "ABOUT";
     header.classList.add("title");
     main.appendChild(header);
 
-    
+    main.appendChild(showBiography('niko'));
+    main.appendChild(showBiography('klonoa'));
 }
 
 export { showAbout };
